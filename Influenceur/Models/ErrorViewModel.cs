fd0a13e40cb7bb8fd572f59aa4fd20375eeb7db6
warsflax <hussamessmahi2@gmail.com>
2025-01-25 19:41:01 +0100

    first commit

---
+++ Influenceur/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Influenceur.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
